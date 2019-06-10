233d2b2124bbd0b4f9d46fbcc9ed12bb247289d8
PPRoche <paul.jp.roche@gmail.com>
2019-06-10 15:06:01 -0400

    created the Map class which creates instances of maps

--- notuber/maps.js
+++ notuber/maps.js
@@ -1,1 +1,9 @@
 //notuber js file
+//Copied & pasted constructor of Map object
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
