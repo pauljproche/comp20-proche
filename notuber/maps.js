c8e5627f5c694de92fc4c569ae546999ea849d05
PPRoche <paul.jp.roche@gmail.com>
2019-06-22 12:18:01 -0400

    fixed path displaying incorrectly

--- notuber/maps.js
+++ notuber/maps.js
@@ -1,51 +1,4 @@
 //notuber js file
-<<<<<<< HEAD
-
-//declaring global scope variables
-var map;
-<<<<<<< HEAD
-var marker;
-=======
->>>>>>> test-branch
-//Default lat and long
-var my_lat = -99999;
-var my_lng = -99999;
-//Default locations of cars
-var locations = [
-    ['mXfkjrFw', 42.3453, -71.0464, 1],
-    ['nZXB8ZHz', 42.3662, -71.0621, 2],
-    ['Tkwu74WC', 42.3603, -71.0547, 3],
-    ['5KWpnAJN', 42.3472, -71.0802, 4],
-    ['uf5ZrXYw', 42.3663, -71.0544, 5],
-    ['VMerzMH8', 42.3542, -71.0704, 6],
-];
-<<<<<<< HEAD
-//Initialize infoWindow
-//var infowindow = new google.maps.InfoWindow();
-=======
-
-var http = new XMLHttpRequest();
-var url = 'https://hans-moleman.herokuapp.com/rides';
-var params = "username=prOuKReR&lat=" + my_lat + "&lng=" + my_lng;
-http.open('POST', url, true);
->>>>>>> test-branch
-
-//Send the proper header information along with the request
-http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-
-http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-    }
-}
-http.send(params);
-
-function initMap() { 
-   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.352271, lng: -71.05524200000001},
-    zoom: 11
-   });
-=======
 //declaring map in global scope for setMap to work
 var map;
 var marker;
@@ -67,7 +20,6 @@ function initMap() {
     zoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
->>>>>>> my-temporary-work
 
 
   
@@ -114,16 +66,7 @@ function getLocation() {
 function renderMap(my_lat, my_lng, contentString){
   console.log(contentString);
   var me = new google.maps.LatLng(my_lat, my_lng);
-<<<<<<< HEAD
-  // Update map and go there...
-<<<<<<< HEAD
-        map.panTo(me);
-        
-=======
->>>>>>> test-branch
-=======
   // Update map and go there...        
->>>>>>> my-temporary-work
         // Create a marker
         marker = new google.maps.Marker({
           position: me,
@@ -131,20 +74,7 @@ function renderMap(my_lat, my_lng, contentString){
           title: shortest_id, 
           content: contentString
         });
-<<<<<<< HEAD
         marker.setMap(map);
-<<<<<<< HEAD
-
-        // Open info window on click of marker
-        //google.maps.event.addListener(marker, 'click', function() {
-        //  infowindow.setContent(marker.title);
-        //  infowindow.open(map, marker);
-        //});
-          
-=======
-        marker.setMap(map);      
->>>>>>> test-branch
-=======
         console.log("HERE");
         var infowindow = new google.maps.InfoWindow();
         // Open info window on click of markerg 0.   
@@ -275,6 +205,5 @@ function createPath(){
   });
   travelPath.setMap(map);                         
   
->>>>>>> my-temporary-work
 }
 //CHEERIOS
